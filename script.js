309cdcae6a58c8f6c24093bac71c43e068a5241c
limaocomvodka <104787948+limaocomvodka@users.noreply.github.com>
2022-06-17 14:46:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = "Limão"
+console.log(nome);
+
+let numero = 31
+console.log(numero);
+
+let ano = 2022
+console.log(ano);
+
+console.log (ano - numero);
+console.log (ano + numero);
+
+console.log("Estou mexendo no console");
+
+let quartaF = false
+
+console.log(Boolean)
+console.log (typeof quartaF)
